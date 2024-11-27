576b90503176c58f39872c70378e3048d9181181
AyelenPorqueres <ayelen.porqueres04@gmail.com>
2024-11-27 15:56:21 -0300

    modificaciones en el diseño de pieChart

--- src/app/components/pieChart.tsx
+++ src/app/components/pieChart.tsx
@@ -6,26 +6,30 @@ import { useTranslation } from 'next-i18next';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  labels: ['Amazon', 'Google', 'Meta', 'Microsoft', 'Novartis', 'Pepsi', 'Visa', 'Walmart'],
   datasets: [
     {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      label: '',
+      data: [20, 15, 10, 25, 5, 10, 8, 7],
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
+        'rgba(242,163,32)',//amazon
+        'rgba(216,65,46,255)',//google
+        'rgba(39,126,217)',//meta
+        'rgba(140,196,63)',//microsoft
+        'rgba(226,75,30,255)',//novartis
+        'rgba(1,75,148,255)',//pepsi
+        'rgba(5,107,179,255)',//visa
+        'rgba(254,186,51,255)',//walmart
       ],
       borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
+        'rgba(242,163,32)',//amazon
+        'rgba(216,65,46,255)',//google
+        'rgba(39,126,217)',//meta
+        'rgba(140,196,63)',//microsoft
+        'rgba(226,75,30,255)',//novartis
+        'rgba(1,75,148,255)',//pepsi
+        'rgba(5,107,179,255)', //visa
+        'rgba(254,186,51,255)',//walmart
       ],
       borderWidth: 1,
     },
@@ -33,13 +37,35 @@ export const data = {
 };
 
 export function PieChart() {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   return (
-  <div className="mb-10 mr-6 mt-5 bg-[rgb(243,246,249)] shadow-lg rounded w-[40%]">
-  <p className='pt-5 ml-2 text-lg font-bold'>{t('titlePie')}</p>
-  <div className="h-[1px] bg-slate-400 ml-2 mr-2 mt-2"></div>
-  <Pie data={data} />
-  </div>
+    <div className="mb-10 mr-6 mt-5 bg-[rgb(243,246,249)] shadow-lg rounded w-[40%]">
+      <p className='pt-5 ml-2 text-lg font-bold'>{t('titlePie')}</p>
+      <div className="h-[1px] bg-slate-400 ml-2 mr-2 mt-2"></div>
+      <div className='w-[480px] h-[480px] ml-10 p-9'>
+        <Pie
+          data={data}
+          options={{
+            plugins: {
+              legend: {
+                position: 'right',
 
-)
+                labels: {
+                  padding: 15,
+                  usePointStyle: true,
+                  pointStyle: 'circle',
+                  font: {
+                    size: 15, 
+                  }
+                }
+              }
+            }
+          }
+          }
+
+        />
+      </div>
+    </div>
+
+  )
 }
